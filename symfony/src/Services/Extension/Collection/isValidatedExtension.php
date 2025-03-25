b64c5505a1f5c477907490dcb00c4ddb8f671d1c
Yohann Valentin <pro@yohannvalentin.com>
2025-03-25 15:49:07 +0100

    fix(extension): support editor role in addValidated method from applyToItem

--- symfony/src/Services/Extension/Collection/isValidatedExtension.php
+++ symfony/src/Services/Extension/Collection/isValidatedExtension.php
@@ -27,7 +27,7 @@ final readonly class isValidatedExtension implements QueryCollectionExtensionInt
 
     public function applyToItem(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, array $identifiers, ?Operation $operation = null, array $context = []): void
     {
-        $this->addValidated($queryBuilder, $resourceClass);
+        $this->addValidated($queryBuilder, $resourceClass, true);
     }
 
     private function isSupport(string $resourceClass): bool
@@ -35,9 +35,13 @@ final readonly class isValidatedExtension implements QueryCollectionExtensionInt
         return in_array($resourceClass, [Actor::class, Resource::class, Project::class]);
     }
 
-    private function addValidated(QueryBuilder $queryBuilder, string $resourceClass): void
+    private function addValidated(QueryBuilder $queryBuilder, string $resourceClass, bool $createdBySupported = false): void
     {
-        if (!$this->isSupport($resourceClass) || $this->security->isGranted(UserRoles::ROLE_ADMIN)) {
+        if (
+            !$this->isSupport($resourceClass)
+            || $this->security->isGranted(UserRoles::ROLE_ADMIN)
+            || ($createdBySupported && $this->security->isGranted(UserRoles::ROLE_EDITOR_RESSOURCES))
+        ) {
             return;
         }
 
