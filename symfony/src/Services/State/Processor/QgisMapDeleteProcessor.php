abad4e52e55eefd0146e3abd0373e4d04576f6d5
0live <0live@users.noreply.github.com>
2025-06-04 20:05:43 +0000

    Fix styling

--- symfony/src/Services/State/Processor/QgisMapDeleteProcessor.php
+++ symfony/src/Services/State/Processor/QgisMapDeleteProcessor.php
@@ -13,7 +13,8 @@ class QgisMapDeleteProcessor implements ProcessorInterface
 {
     public function __construct(
         private readonly EntityManagerInterface $entityManager,
-    ) {}
+    ) {
+    }
 
     public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): void
     {
@@ -22,10 +23,7 @@ class QgisMapDeleteProcessor implements ProcessorInterface
         }
 
         if (!$data->getAtlases()->isEmpty()) {
-            throw new HttpException(
-                Response::HTTP_INTERNAL_SERVER_ERROR,
-                'Cannot delete QgisMap: it is associated with one or more Atlas'
-            );
+            throw new HttpException(Response::HTTP_INTERNAL_SERVER_ERROR, 'Cannot delete QgisMap: it is associated with one or more Atlas');
         }
 
         $this->entityManager->remove($data);
