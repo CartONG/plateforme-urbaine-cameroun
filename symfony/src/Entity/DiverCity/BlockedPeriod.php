f66e4a41499a5e491282ef743d4986ad0b3967ff
kz237 <kz237@users.noreply.github.com>
2026-08-29 14:39:24 +0000

    Fix styling

--- symfony/src/Entity/DiverCity/BlockedPeriod.php
+++ symfony/src/Entity/DiverCity/BlockedPeriod.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity\DiverCity;
 
+use ApiPlatform\Doctrine\Orm\Filter\BooleanFilter;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
 use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Delete;
@@ -9,8 +11,6 @@ use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Patch;
 use ApiPlatform\Metadata\Post;
-use ApiPlatform\Doctrine\Orm\Filter\BooleanFilter;
-use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
 use App\Entity\User\User;
 use App\Repository\DiverCity\BlockedPeriodRepository;
 use App\Security\Voter\DiverCity\SpaceScopedVoter;
