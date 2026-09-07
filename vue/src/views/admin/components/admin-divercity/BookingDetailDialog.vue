166959febae65cafa76f44ad637cb195964eae71
Arnold KEZETER <kz.geosm@gmail.com>
2026-09-07 14:59:48 +0100

    retrait définitif du motif de la réservation

--- vue/src/views/admin/components/admin-divercity/BookingDetailDialog.vue
+++ vue/src/views/admin/components/admin-divercity/BookingDetailDialog.vue
@@ -18,7 +18,6 @@
         <p v-if="eventActivityTypeLabel">
           <strong>{{ $t('divercity.admin.detail.activityType') }} :</strong> {{ eventActivityTypeLabel }}
         </p>
-        <p><strong>{{ $t('divercity.admin.detail.purpose') }} :</strong> {{ booking.bookingPurpose }}</p>
         <p v-if="booking.additionalInformation">
           <strong>{{ $t('divercity.admin.detail.additionalInfo') }} :</strong> {{ booking.additionalInformation }}
         </p>
